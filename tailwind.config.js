d891eb624bf2e48c8cce51c214c3b27d2f05afc3
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-05-10 21:57:29 -0700

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
 	purge: [
-		'./src/components/**/*.{js,ts,jsx,tsx}',
-		'./src/pages/**/*.{js,ts,jsx,tsx}',
+		[
+			'./pages/**/*.{js,ts,jsx,tsx}',
+			'./components/**/*.{js,ts,jsx,tsx}',
+			'./layouts/**/*.{js,ts,jsx,tsx}',
+		],
 	],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
