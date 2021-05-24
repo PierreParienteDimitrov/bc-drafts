e83de887a2fbba5d4a30649e5f50295e45edcda5
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-05-24 15:56:29 -0700

    added segment.com snippet

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,14 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html>
-				<Head />
+				<Head>
+					{' '}
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="lLqmz3Mgbk0eyEi1M5GMpigKVfrCbq7f";analytics.SNIPPET_VERSION="4.13.2"; analytics.load("lLqmz3Mgbk0eyEi1M5GMpigKVfrCbq7f"); analytics.page(); }}();`,
+						}}
+					/>
+				</Head>
 				<link rel='preconnect' href='https://fonts.gstatic.com' />
 				<link
 					href='https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
